9b57cd201315ab03eeae8083ed8b4dbb383d1102
neurospeech <support@neurospeech.com>
2016-07-21 16:06:12 +0530

    wip

--- TestApp/Properties/AssemblyInfo.cs
+++ TestApp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.ShortcutBadger")]
+[assembly: AssemblyTitle("TestApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.ShortcutBadger")]
+[assembly: AssemblyProduct("TestApp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,4 +26,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.808")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- XamarinShortcutBadger/Badger.cs
+++ XamarinShortcutBadger/Badger.cs
@@ -10,29 +10,30 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
-namespace XamarinShortcutBadger
-{
-    /// <summary>
-    /// Sets shortcut badges...
-    /// </summary>
-    public class Badger
-    {
+//namespace XamarinShortcutBadger
+//{
+//    /// <summary>
+//    /// Sets shortcut badges...
+//    /// </summary>
+//    public class Badger
+//    {
 
-        /// <summary>
-        /// Creates a badge
-        /// </summary>
-        /// <param name="context">Application context</param>
-        /// <param name="count">badge count</param>
-        public static void ApplyCount(Context context, int count) {
-            ME.Leolin.Shortcutbadger.ShortcutBadger.With(context).Count(count);
-        }
+//        /// <summary>
+//        /// Creates a badge
+//        /// </summary>
+//        /// <param name="context">Application context</param>
+//        /// <param name="count">badge count</param>
+//        public static void ApplyCount(Context context, int count) {
+            
+//            //ME.Leolin.Shortcutbadger.ShortcutBadger..With(context).Count(count);
+//        }
 
-        /// <summary>
-        /// Removes badge
-        /// </summary>
-        /// <param name="context"></param>
-        public static void Remove(Context context) {
-            ME.Leolin.Shortcutbadger.ShortcutBadger.With(context).Remove();
-        }
-    }
-}
+//        /// <summary>
+//        /// Removes badge
+//        /// </summary>
+//        /// <param name="context"></param>
+//        public static void Remove(Context context) {
+//            //ME.Leolin.Shortcutbadger.ShortcutBadger.With(context).Remove();
+//        }
+//    }
+//}
