becbb1a77cf44dc740fc853a1d1ef5dad2df0f8d
Akash Kava <support@neurospeech.com>
2017-09-07 01:04:55 +0530

    Version of ShortcutBadger updated

--- XamarinShortcutBadger/Properties/AssemblyInfo.cs
+++ XamarinShortcutBadger/Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.808")]
+[assembly: AssemblyVersion("1.1.982")]
